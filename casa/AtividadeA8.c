fe0d177fb6a9217200f5d116363e0ed767510298
Gabriel Oliveira Miranda <giane.ga2010@gmail.com>
2025-08-27 13:13:03 -0300

    mais codigos assembly

--- casa/AtividadeA8.c
+++ casa/AtividadeA8.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int a = 10, b = 6, c = 6, result;
+    int a = 10, b = 6, c = 6, result = 0;
+
 
     __asm__ (
-        "movl %1, %%eax;"
-        "movl %2, %%ebx;"
-        "movl %3, %%ecx;"
-        "addl %%ebx, %%eax;"
-        "movl %%eax, %0;"
-        "addl %%ecx, %%eax;"
-        "movl %%eax, %0;"
-        : "=r" (result)
-        : "r" (a), "r" (b), "r" (c)
-        : "%eax", "%ebx", "ecx"
+        "addl %%ebx, %%eax;"            // eax = eax + ebx
+        "addl %%ecx, %%eax;"            // eax = eax + ecx
+        : "=a" (result)                 // output: em eax → result
+        : "a" (a), "b" (b), "c" (c)     // fixed inputs: eax, ebx, ecx
     );
+
 
     printf("%d + %d + %d = %d", a, b, c, result);
 
